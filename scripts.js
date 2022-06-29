47818cdef003725d48d5b781dcb8ba3c62d1bf3b
Miran-Mirantee <killerm11805@hotmail.com>
2022-06-29 23:07:01 +0700

    Added createGrid() function and button for creating new grid

--- scripts.js
+++ scripts.js
@@ -6,16 +6,29 @@ const container = document.createElement('div');
 container.classList.add('container');
 canvas.appendChild(container);
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    for (let j = 0; j < 16; j++) {
-        const pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        pixel.addEventListener('click', () => {
-            pixel.setAttribute('style', 'background-color: black;');
-        });
-        row.appendChild(pixel);
+function createGrid(grid) {
+    for (let i = 0; i < grid; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let j = 0; j < grid; j++) {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            pixel.addEventListener('click', () => {
+                pixel.setAttribute('style', 'background-color: black;');
+            });
+            row.appendChild(pixel);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
+
+let grid = 16;
+createGrid(grid);
+
+
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', () => {
+    grid = prompt(`Enter a number of square per row:`);
+    createGrid(grid);
+    console.log(grid);
+});
