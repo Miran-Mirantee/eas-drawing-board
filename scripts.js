c98d17b1dd6e21764460b6fb8c3e7fc9b0b50b8c
Miran-Mirantee <killerm11805@hotmail.com>
2022-06-29 22:53:06 +0700

    Implement drawing system

--- scripts.js
+++ scripts.js
@@ -2,13 +2,20 @@ const canvas = document.createElement('div');
 canvas.classList.add('canvas');
 document.body.appendChild(canvas);
 
+const container = document.createElement('div');
+container.classList.add('container');
+canvas.appendChild(container);
+
 for (let i = 0; i < 16; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
     for (let j = 0; j < 16; j++) {
         const pixel = document.createElement('div');
         pixel.classList.add('pixel');
+        pixel.addEventListener('click', () => {
+            pixel.setAttribute('style', 'background-color: black;');
+        });
         row.appendChild(pixel);
     }
-    canvas.appendChild(row);
+    container.appendChild(row);
 }
